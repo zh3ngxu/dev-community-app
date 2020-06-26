99b15c2a02e2099fb13d68c662900d7896872aca
Zheng <ZhengXu94@users.noreply.github.com>
2020-06-25 20:12:22 -0400

    post added

---
+++ models/Post.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      users: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        requried: true,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        deffault: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    deffault: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
