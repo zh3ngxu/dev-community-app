623f96cda4c460d109e1a1a8592a1f23d6bf1e14
Zheng <ZhengXu94@users.noreply.github.com>
2020-06-28 12:58:13 -0400

    GET PROFILE added

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
 // import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
-export const Register = () => {
+export const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,9 +22,9 @@ export const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     } else {
-      console.log(' SUCCESS');
+      register({ name, email, password });
       // const newUser = {
       //   name,
       //   email,
@@ -44,6 +48,10 @@ export const Register = () => {
     }
   };
 
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
@@ -58,7 +66,7 @@ export const Register = () => {
             name='name'
             value={name}
             onChange={(e) => onChange(e)}
-            required
+            // required
           />
         </div>
         <div className='form-group'>
@@ -68,7 +76,7 @@ export const Register = () => {
             name='email'
             value={email}
             onChange={(e) => onChange(e)}
-            required
+            // required
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
@@ -82,7 +90,7 @@ export const Register = () => {
             name='password'
             value={password}
             onChange={(e) => onChange(e)}
-            minLength='6'
+            // minLength='6'
           />
         </div>
         <div className='form-group'>
@@ -92,7 +100,7 @@ export const Register = () => {
             name='password2'
             value={password2}
             onChange={(e) => onChange(e)}
-            minLength='6'
+            // minLength='6'
           />
         </div>
         <input type='submit' className='btn btn-primary' value='Register' />
@@ -104,4 +112,14 @@ export const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// add a globe header
+
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+  }
+};
+
+export default setAuthToken;
